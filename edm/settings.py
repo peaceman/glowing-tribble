021dba11183baa17272dd8b09ca63a99d6ee1f3d
peaceman <nixalio@gmail.com>
2013-11-11 00:33:15 +0100

    installed django-bower

--- edm/settings.py
+++ edm/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'south'
+    'south',
+    'djangobower',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,3 +82,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_FINDERS = (
+    'djangobower.finders.BowerFinder',
+)
+
+BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, 'components')
+BOWER_INSTALLED_APPS = (
+    'jquery',
+    'lodash',
+)
