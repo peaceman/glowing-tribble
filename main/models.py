dda885c6f22d7b6f594845c75d830db3b5ac08dc
peaceman <nixalio@gmail.com>
2013-11-13 00:00:32 +0100

    defined MusicSoftware model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class MusicSoftware(models.Model):
+	name = models.CharField(max_length=255, unique=True)
+	is_active = models.BooleanField(default=False)
+	created_by = models.ForeignKey(User, db_column='created_by')
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
