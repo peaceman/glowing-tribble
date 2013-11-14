4ce20b32e02f9c9a0ff4012f73af9b4b15a5fbf5
peaceman <nixalio@gmail.com>
2013-11-14 05:36:42 +0100

    code reformat

--- main/models.py
+++ main/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class MusicSoftware(models.Model):
-	name = models.CharField(max_length=255, unique=True)
-	is_active = models.BooleanField(default=False)
-	created_by = models.ForeignKey(User, db_column='created_by')
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, db_column='created_by')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- main/views.py
+++ main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-	return render(request, 'main/index.html')
+    return render(request, 'main/index.html')
