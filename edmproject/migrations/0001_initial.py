a6c37d085f831b454383816084eb91eecb5807a9
peaceman <nixalio@gmail.com>
2013-11-17 05:21:51 +0100

    migrations for edmproject app

---
+++ edmproject/migrations/0001_initial.py
@@ -0,0 +1,225 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'ProjectFileCategory'
+        db.create_table(u'edmproject_projectfilecategory', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=255, db_index=True)),
+            ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('updated_at', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+        ))
+        db.send_create_signal(u'edmproject', ['ProjectFileCategory'])
+
+        # Adding model 'ProjectFile'
+        db.create_table(u'edmproject_projectfile', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('alive', self.gf('edm.models.LiveField')(default=True, null=True)),
+            ('category', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['edmproject.ProjectFileCategory'])),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=50)),
+            ('author', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], db_column='created_by')),
+        ))
+        db.send_create_signal(u'edmproject', ['ProjectFile'])
+
+        # Adding model 'ProjectFileVersion'
+        db.create_table(u'edmproject_projectfileversion', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('project_file', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['edmproject.ProjectFile'])),
+            ('review', self.gf('django.db.models.fields.related.OneToOneField')(related_name='review_of', unique=True, to=orm['edmreview.Review'])),
+            ('price', self.gf('django.db.models.fields.DecimalField')(max_digits=9, decimal_places=2)),
+            ('description', self.gf('django.db.models.fields.TextField')()),
+            ('bpm', self.gf('django.db.models.fields.PositiveSmallIntegerField')()),
+            ('session', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['edmuser.UserSession'])),
+            ('genre', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['edmmusic.MusicGenre'])),
+            ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('updated_at', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+        ))
+        db.send_create_signal(u'edmproject', ['ProjectFileVersion'])
+
+        # Adding M2M table for field compatible_programs on 'ProjectFileVersion'
+        m2m_table_name = db.shorten_name(u'edmproject_projectfileversion_compatible_programs')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('projectfileversion', models.ForeignKey(orm[u'edmproject.projectfileversion'], null=False)),
+            ('musicprogram', models.ForeignKey(orm[u'edmmusic.musicprogram'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['projectfileversion_id', 'musicprogram_id'])
+
+        # Adding M2M table for field compatible_plugins on 'ProjectFileVersion'
+        m2m_table_name = db.shorten_name(u'edmproject_projectfileversion_compatible_plugins')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('projectfileversion', models.ForeignKey(orm[u'edmproject.projectfileversion'], null=False)),
+            ('musicplugins', models.ForeignKey(orm[u'edmmusic.musicplugins'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['projectfileversion_id', 'musicplugins_id'])
+
+        # Adding M2M table for field compatible_banks on 'ProjectFileVersion'
+        m2m_table_name = db.shorten_name(u'edmproject_projectfileversion_compatible_banks')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('projectfileversion', models.ForeignKey(orm[u'edmproject.projectfileversion'], null=False)),
+            ('musicbanks', models.ForeignKey(orm[u'edmmusic.musicbanks'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['projectfileversion_id', 'musicbanks_id'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'ProjectFileCategory'
+        db.delete_table(u'edmproject_projectfilecategory')
+
+        # Deleting model 'ProjectFile'
+        db.delete_table(u'edmproject_projectfile')
+
+        # Deleting model 'ProjectFileVersion'
+        db.delete_table(u'edmproject_projectfileversion')
+
+        # Removing M2M table for field compatible_programs on 'ProjectFileVersion'
+        db.delete_table(db.shorten_name(u'edmproject_projectfileversion_compatible_programs'))
+
+        # Removing M2M table for field compatible_plugins on 'ProjectFileVersion'
+        db.delete_table(db.shorten_name(u'edmproject_projectfileversion_compatible_plugins'))
+
+        # Removing M2M table for field compatible_banks on 'ProjectFileVersion'
+        db.delete_table(db.shorten_name(u'edmproject_projectfileversion_compatible_banks'))
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'edmmusic.musicbanks': {
+            'Meta': {'object_name': 'MusicBanks'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'plugin': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['edmmusic.MusicPlugins']"}),
+            'reviewed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'session': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['edmuser.UserSession']"}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        u'edmmusic.musicgenre': {
+            'Meta': {'object_name': 'MusicGenre'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'reviewed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'session': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['edmuser.UserSession']"}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        u'edmmusic.musicplugins': {
+            'Meta': {'object_name': 'MusicPlugins'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'reviewed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'session': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['edmuser.UserSession']"}),
+            'software': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['edmmusic.MusicProgram']"}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        u'edmmusic.musicprogram': {
+            'Meta': {'object_name': 'MusicProgram'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'reviewed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'session': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['edmuser.UserSession']"}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        u'edmproject.projectfile': {
+            'Meta': {'object_name': 'ProjectFile'},
+            'alive': ('edm.models.LiveField', [], {'default': 'True', 'null': 'True'}),
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']", 'db_column': "'created_by'"}),
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['edmproject.ProjectFileCategory']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'edmproject.projectfilecategory': {
+            'Meta': {'object_name': 'ProjectFileCategory'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        u'edmproject.projectfileversion': {
+            'Meta': {'object_name': 'ProjectFileVersion'},
+            'bpm': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
+            'compatible_banks': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['edmmusic.MusicBanks']", 'symmetrical': 'False'}),
+            'compatible_plugins': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['edmmusic.MusicPlugins']", 'symmetrical': 'False'}),
+            'compatible_programs': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['edmmusic.MusicProgram']", 'symmetrical': 'False'}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'genre': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['edmmusic.MusicGenre']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'price': ('django.db.models.fields.DecimalField', [], {'max_digits': '9', 'decimal_places': '2'}),
+            'project_file': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['edmproject.ProjectFile']"}),
+            'review': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'review_of'", 'unique': 'True', 'to': u"orm['edmreview.Review']"}),
+            'session': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['edmuser.UserSession']"}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        u'edmreview.review': {
+            'Meta': {'object_name': 'Review'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'result': ('django.db.models.fields.NullBooleanField', [], {'default': 'None', 'null': 'True', 'blank': 'True'}),
+            'result_reasoning': ('django.db.models.fields.TextField', [], {'null': 'True'}),
+            'state': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '1', 'db_index': 'True'}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        u'edmuser.useragent': {
+            'Meta': {'object_name': 'UserAgent'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'value': ('django.db.models.fields.TextField', [], {})
+        },
+        u'edmuser.usersession': {
+            'Meta': {'object_name': 'UserSession'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ip_address': ('django.db.models.fields.GenericIPAddressField', [], {'max_length': '39'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"}),
+            'user_agent': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['edmuser.UserAgent']"})
+        }
+    }
+
+    complete_apps = ['edmproject']
