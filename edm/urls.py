090153a27ed6bdd6d454ef69d4ffd6a9c9034f94
peaceman <nixalio@gmail.com>
2013-11-20 01:12:57 +0100

    layout, merge of login and signup templates

--- edm/urls.py
+++ edm/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
     url(r'^$', 'main.views.home', name='home'),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^account/signup/$', edmuser.views.SignUpView.as_view(), name='account_signup'),
+    url(r'^account/login/$', edmuser.views.LoginView.as_view(), name='account_login'),
     url(r'^account/', include('account.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- edmuser/views.py
+++ edmuser/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from account.forms import LoginUsernameForm
 import account.views
 import edmuser.forms
+from edmuser.forms import SignupForm
+
+
+class LoginView(account.views.LoginView):
+    template_name = 'account/login_signup.html'
+
+    def render_to_response(self, context, **response_kwargs):
+        forms = dict(signup_form=edmuser.forms.SignupForm, login_form=self.get_form(self.get_form_class()))
+        context = dict(context.items() + forms.items())
+        return super(LoginView, self).render_to_response(context, **response_kwargs)
 
 
 class SignUpView(account.views.SignupView):
     form_class = edmuser.forms.SignupForm
+    template_name = 'account/login_signup.html'
+
+    def render_to_response(self, context, **response_kwargs):
+        forms = dict(signup_form=self.get_form(self.get_form_class()), login_form=LoginUsernameForm)
+        context = dict(context.items() + forms.items())
+        return super(SignUpView, self).render_to_response(context, **response_kwargs)
 
     def create_user(self, form, commit=True, **kwargs):
         names = {
